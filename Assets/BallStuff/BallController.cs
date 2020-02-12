9fea7570628c5c10f2fc367b383357c22b525324
Brennen Green <brennengreen@outlook.com>
2020-02-11 22:38:29 -0500

    First

---
+++ Assets/BallStuff/BallController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+public class BallController : MonoBehaviour
+{
+    static float[] reflection = { -1.0f, 1.0f };
+    float vx;
+    float vy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        vx = Random.Range(.1f, .25f) * reflection[(int)Random.Range(0, 2)] * 2;
+        vy = Random.Range(.1f, .25f) * reflection[(int)Random.Range(0, 2)] * 2;
+        transform.position = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = transform.position + new Vector3(vx, 0, vy);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Collided With: " + other.gameObject.tag);
+        if (other.gameObject.CompareTag("SouthWall") || other.gameObject.CompareTag("NorthWall"))
+        {
+            vy = -vy;
+        }
+        else if (other.gameObject.CompareTag("EastWall") || other.gameObject.CompareTag("WestWall"))
+        {
+            vx = -vx;
+        }
+    }
+}
